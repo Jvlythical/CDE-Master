73b9e52c34c047bdbfe3010f4ad2872cea3d4667
Michael Yen <lvlichael@gmail.com>
2018-08-18 00:40:00 +0000

    Update master structure

--- master/start-db-console.sh
+++ master/start-db-console.sh
@@ -6,10 +6,10 @@ database_conf_path=$rails_root/config/database.yml
 schema_path=$rails_root/db/schema.rb
 
 # Master
-docker run -it --name cde-db-console-test \
+docker run -it --name cde-db-console \
 --link cde-database-production:cde-database-production \
 -v $(pwd)/database.yml:$database_conf_path \
 -v $(pwd)/schema.rb:$schema_path \
 -e "DRIVES_ROOT=$drives_path" -e "SYSTEM_ROOT=$system_path" \
 -e "MAILGUN_DOMAIN=mail.kodethon.com" -e "MAILGUN_API_KEY=key-b4919c68dcbf8cd3dfc16fc6198d1641" \
-jvlythical/cde-master:2.9.16-rc sh -c '/bin/bash'
+jvlythical/cde-master:3.8.0-rc sh -c '/bin/bash'
--- master/start-master-pro.sh
+++ master/start-master-pro.sh
@@ -6,17 +6,28 @@ database_conf_path=$rails_root/config/database.yml
 production_log_path=$rails_root/log/production.log
 schema_path=$rails_root/db/schema.rb
 
+# Export ENV variables
+export $(sed -e 's/:[^:\/\/]/=/g;s/$//g;s/ *=/=/g' ../config/credentials.yml)
+
+if [ -z $MAILGUN_DOMAIN ]; then
+	echo "MAILGUN_DOMAIN is not set."
+	exit
+fi
+
+if [ -z $MAILGUN_API_KEY ]; then
+	echo "MAILGUN_API_KEY is not set."
+	exit
+fi
+
 # Master
 docker run -d --name cde-master -p 8080:8080 \
 --link cde-cache:memcache \
 --link cde-database-production:cde-database-production \
--v $(pwd)/database.yml:$database_conf_path \
+-v $(pwd)/../config/database.yml:$database_conf_path \
 -v $(pwd)/logs/production.log:$production_log_path \
 -v $(pwd)/../ssl/ssl-bundle.crt:/etc/ssl/ssl-bundle.crt \
 -v $(pwd)/../ssl/kodethon.key:/etc/ssl/kodethon.key \
 -v $(pwd)/schema.rb:$schema_path \
- -v $(pwd)/overrides/assignment_controller.rb:$rails_root/app/controllers/assignment_controller.rb \
 -e "DRIVES_ROOT=$drives_path" -e "SYSTEM_ROOT=$system_path" -e "RAILS_ENV=production" \
--e "MAILGUN_DOMAIN=mail.kodethon.com" -e "MAILGUN_API_KEY=key-b4919c68dcbf8cd3dfc16fc6198d1641" \
-jvlythical/cde-master:2.9.16-rc sh -c '/sbin/run.sh'
-
+-e "MAILGUN_DOMAIN=$MAILGUN_DOMAIN" -e "MAILGUN_API_KEY=$MAILGUN_API_KEY" \
+jvlythical/cde-master:3.8.0-rc sh -c '/sbin/run.sh'
