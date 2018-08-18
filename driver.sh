56bae1fc720b53f8b1f98409e0bded1322d9deee
Michael Yen <lvlichael@gmail.com>
2018-08-18 12:03:57 +0000

    Add logging to driver.sh

--- driver.sh
+++ driver.sh
@@ -1,3 +1,5 @@
-cd cache && sh start-cache.sh; cd ..
-cd frontend && sh start-frontend-pro.sh; cd ..
+cd cache && sh start-cache.sh 2> /dev/null; cd .. 
+echo 'Starting frontend container...'
+cd frontend && sh start-frontend-pro.sh 2> /dev/null; cd ..
+echo 'Starting master container...'
 cd master && sh start-master-pro.sh
