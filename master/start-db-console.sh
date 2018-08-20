45822f931b01d97dc32f8d84069248f4955e56de
Michael Yen <lvlichael@gmail.com>
2018-08-20 07:37:02 +0000

    Update start-db-console.sh

--- master/start-db-console.sh
+++ master/start-db-console.sh
@@ -5,11 +5,24 @@ system_path=$rails_root/private/system
 database_conf_path=$rails_root/config/database.yml
 schema_path=$rails_root/db/schema.rb
 
+# Export ENV variables
+export $(sed -e 's/:[^:\/\/]/=/g;s/$//g;s/ *=/=/g' ../config/credentials.yml)
+
+if [ -z $MAILGUN_DOMAIN ]; then
+	echo "MAILGUN_DOMAIN is not set."
+	exit
+fi
+
+if [ -z $MAILGUN_API_KEY ]; then
+	echo "MAILGUN_API_KEY is not set."
+	exit
+fi
+
 # Master
 docker run -it --name cde-db-console \
 --link cde-database-production:cde-database-production \
--v $(pwd)/database.yml:$database_conf_path \
+-v $(pwd)/../config/database.yml:$database_conf_path \
 -v $(pwd)/schema.rb:$schema_path \
 -e "DRIVES_ROOT=$drives_path" -e "SYSTEM_ROOT=$system_path" \
--e "MAILGUN_DOMAIN=mail.kodethon.com" -e "MAILGUN_API_KEY=key-b4919c68dcbf8cd3dfc16fc6198d1641" \
-jvlythical/cde-master:3.8.0-rc sh -c '/bin/bash'
+-e "MAILGUN_DOMAIN=$MAILGUN_DOMAIN" -e "MAILGUN_API_KEY=$MAINGUN_API_KEY" \
+jvlythical/cde-master:3.9.0-rc sh -c '/bin/bash'
