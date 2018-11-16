c69b3689e746003dee820cc6c0d648b8007af1a1
Michael Yen <lvlichael8@gmail.com>
2018-11-16 12:27:32 +0000

    Add init.sh

---
+++ init.sh
@@ -0,0 +1,42 @@
+if [ -z $(which docker) ]; then
+	# Install docker
+	sudo apt-get update
+	sudo apt-get install \
+	    apt-transport-https \
+	    ca-certificates \
+	    curl \
+	    software-properties-common
+	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	sudo add-apt-repository \
+	   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+	   $(lsb_release -cs) \
+	   stable"
+	sudo apt-get update
+	sudo apt-get install -y docker-ce=17.09.0~ce-0~ubuntu 
+	sudo usermod -aG docker $USER
+	echo 'Please logout and then log back in.'	
+	exit
+fi
+
+if [ -z $(ls /usr/local/bin/docker-compose) ]; then
+	# Install docker-compose
+	sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+cd db && docker-compose up -d; cd ..
+cd cache && sh start-cache.sh; cd ..
+cd frontend && sh start-frontend-pro.sh; cd ..
+cd master && sh start-master-pro.sh; cd ..
+
+# If the temp node has not been started, create one
+master_container_name=$(docker ps -lq)
+master_ip_addr=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' $master_container_name 2> /dev/null)
+
+# Update the current default.conf to use the  master's ip addr
+cd load-balancer
+s="\tserver $master_ip_addr fail_timeout=30;\n"
+sed -e "s/__MARKER__/$s/" template.conf > default.conf
+sh start-load-balancer.sh; cd ..
+
+cd reverse-proxy && sh start-reverse-proxy.sh
