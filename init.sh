112c0c26eadfc2f08fa8925b1a8a03c068dc7e4f
Michael Yen <lvlichael8@gmail.com>
2018-11-22 05:47:45 +0000

    Add support for group password in env.yml

--- init.sh
+++ init.sh
@@ -1,9 +1,19 @@
+if [ -z $(ls ../config/env.yml 2> /dev/null) ]; then
+	echo 'Please create config/env.yml'
+	exit
+else
+	# Export ENV variables
+	export $(sed -e 's/:[^:\/\/]/=/g;s/$//g;s/ *=/=/g' ../config/env.yml)
+fi
+
 cd reverse-proxy && sh start-reverse-proxy.sh
 
 cd db && docker-compose up -d; cd ..
 cd cache && sh start-cache.sh; cd ..
 cd frontend && sh start-frontend-pro.sh; cd ..
 cd master && sh start-master-pro.sh; cd ..
+
+docker exec cde-master sh -c "export CDE_GROUP_PASSWORD='$GROUP_PASSWORD'; rake admin:create_cde_group"
 
 # Update the current default.conf to use the  master's ip addr
 cd load-balancer
