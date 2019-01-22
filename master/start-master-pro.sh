83afa1c52fd8a2a602f7e451eddb44814a29a920
Michael Yen <lvlichael@gmail.com>
2019-01-22 08:35:43 +0000

    Add support for github private key

--- master/start-master-pro.sh
+++ master/start-master-pro.sh
@@ -37,6 +37,11 @@ if [ ! -e "$schema_file" ]; then
 	touch "$schema_file"
 fi
 
+github_pkey_file=$(pwd)/../config/private_key.pem
+if [ ! -e "$github_pkey_file" ]; then
+	touch "$github_pkey_file"
+fi
+
 # Container related settings
 rails_root=/usr/share/nginx/html
 drives_path=$rails_root/private/drives
@@ -44,6 +49,7 @@ system_path=$rails_root/private/system
 database_conf_path=$rails_root/config/database.yml
 production_log_path=$rails_root/log/production.log
 schema_path=$rails_root/db/schema.rb
+github_pkey_path=$rails_root/config/private_key.pem
 
 # Master
 docker run -d --name $container_name -h "$(uname -n)" \
@@ -51,11 +57,12 @@ docker run -d --name $container_name -h "$(uname -n)" \
 -v $database_config:$database_conf_path \
 -v $log_file:$production_log_path \
 -v $schema_file:$schema_path \
+-v $github_pkey_file:$github_pkey_path \
 -e "DRIVES_ROOT=$drives_path" -e "SYSTEM_ROOT=$system_path" -e "RAILS_ENV=production" \
 -e "MAILGUN_DOMAIN=$MAILGUN_DOMAIN" -e "MAILGUN_API_KEY=$MAILGUN_API_KEY" \
 -e "STRIPE_PUBLISHABLE_KEY=$STRIPE_PUBLISHABLE_KEY" -e "STRIPE_SECRET_KEY=$STRIPE_SECRET_KEY" \
 -e "MEMCACHE_HOSTNAME=cde-cache" \
-jvlythical/cde-master:4.0.8-rc sh -c '/sbin/run.sh'
+jvlythical/cde-master:4.0.9-rc sh -c '/sbin/run.sh'
 
 #-v $(pwd)/../ssl/ssl-bundle.crt:/etc/ssl/ssl-bundle.crt \
 #-v $(pwd)/../ssl/kodethon.key:/etc/ssl/kodethon.key \
