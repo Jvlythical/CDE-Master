777185653273045311214c71296e7d45da99c4fc
Michael Yen <lvlichael@gmail.com>
2019-09-17 19:18:24 +0000

    Update master container to mount log folder

--- master/start-master-pro.sh
+++ master/start-master-pro.sh
@@ -21,10 +21,9 @@ if [ -z "$container_name" ]; then
 	container_name=cde-master
 fi
 
-log_file=$(pwd)/logs/production.log
-if [ ! -e "$log_file" ]; then
-	mkdir logs 2> /dev/null
-	touch "$log_file"
+log_folder=$(pwd)/log
+if [ ! -e "$log_folder" ]; then
+	mkdir -p "$log_folder"
 fi
 
 database_config=$(pwd)/../config/database.yml
@@ -47,22 +46,23 @@ rails_root=/usr/share/nginx/html
 drives_path=$rails_root/private/drives
 system_path=$rails_root/private/system
 database_conf_path=$rails_root/config/database.yml
-production_log_path=$rails_root/log/production.log
+container_log_path=$rails_root/log
 schema_path=$rails_root/db/schema.rb
 github_pkey_path=$rails_root/config/private_key.pem
-
+image=jvlythical/cde-master:4.8.1-rc
+#image=922f3a271235
 # Master
 docker run -d --name $container_name -h "$(uname -n)" \
 --network docker-internal \
 -v $database_config:$database_conf_path \
--v $log_file:$production_log_path \
+-v $log_folder:$container_log_path \
 -v $schema_file:$schema_path \
 -v $github_pkey_file:$github_pkey_path \
 -e "DRIVES_ROOT=$drives_path" -e "SYSTEM_ROOT=$system_path" -e "RAILS_ENV=production" \
 -e "MAILGUN_DOMAIN=$MAILGUN_DOMAIN" -e "MAILGUN_API_KEY=$MAILGUN_API_KEY" \
 -e "STRIPE_PUBLISHABLE_KEY=$STRIPE_PUBLISHABLE_KEY" -e "STRIPE_SECRET_KEY=$STRIPE_SECRET_KEY" \
 -e "MEMCACHE_HOSTNAME=cde-cache" \
-jvlythical/cde-master:4.2.0-rc sh -c '/sbin/run.sh'
+$image sh -c '/sbin/run.sh'
 
 #-v $(pwd)/../ssl/ssl-bundle.crt:/etc/ssl/ssl-bundle.crt \
 #-v $(pwd)/../ssl/kodethon.key:/etc/ssl/kodethon.key \
