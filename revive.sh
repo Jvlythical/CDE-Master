61a0434c2020c22adc8cd8acf8147fb361caa16b
Michael Yen <lvlichael@gmail.com>
2019-09-17 19:17:16 +0000

    Update revive.sh

--- revive.sh
+++ revive.sh
@@ -1,8 +1,7 @@
 docker start cde-frontend
-docker start cde-database-production
 docker start cde-cache
 docker start cde-master
 docker start cde-load-balancer
 docker start datadog-agent
 sh migrate.sh
-docker start nginx-proxy
+#docker start nginx-proxy
